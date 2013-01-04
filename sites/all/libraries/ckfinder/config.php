25339c342c7a543eef37ece891bcffe2a06476e8
Ruslan Piskarev <ruslan.piskarev@gmail.com>
2013-01-04 17:17:45 +0200

    modules update

--- sites/all/libraries/ckfinder/config.php
+++ sites/all/libraries/ckfinder/config.php
@@ -60,7 +60,9 @@ ATTENTION: The trailing slash is required.
 */
 $baseDir = resolveUrl($baseUrl);
 
-require_once '/var/www/zakonipravo/zakon-i-pravo.net/sites/all/modules/ckeditor/includes/filemanager.config.php';
+//require_once '/var/www/zakonipravo/zakon-i-pravo.net/sites/all/modules/ckeditor/includes/filemanager.config.php';
+require_once '../../../../../includes/filemanager.config.php';
+
 //require_once '/home/steve.test/www/sites/all/modules/ckeditor/includes/filemanager.config.php';
 //require_once '../../modules/ckeditor/includes/filemanager.config.php';
 //require_once '../../../includes/filemanager.config.php';
